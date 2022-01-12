ab503ea99f6d135adc0ebecc601a45c20e3a7f17
lancastmx <angelprep92@gmail.com>
2022-01-12 17:28:10 -0600

    Esta plantilla es para una tienda rapida pero no tiene enlaces para compra

--- header.php
+++ header.php
@@ -1,52 +1,15 @@
-<!DOCTYPE html>
-<html <?php language_attributes();?>>
-
+<!<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo("charset") ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <?php wp_head(); ?>
 </head>
 
 <body>
     <?php wp_body_open(); ?>
-    <header class="encabezado">
-        <div class="container-fluid gx-5 py-3">
-            <div class="row">
-                <div class="encabezado__hamburguesa col-2 col-md-2 px-2 px-md-4">
-                    <a href="#">
-                        <img src="<?php echo get_template_directory_uri()?>/assets/img/icon-menu.svg" alt="menu icon">
-                    </a>
-                </div>
-                <div class="enbezado__logo col-5 col-md-2 px-2 px-md-4">
-                    <?php the_custom_logo(); ?>
-                </div>
-                <div class="encabezado__menu col-6 col-md-5">
-                    <?php wp_nav_menu(
-                        array(
-                            "menu" => 'menu-principal'
-                        )
-                    ); ?>
-                </div>
-                <div class="encabezado__cart col-5 col-md-1">
-                    <a href="sign_in.html" class="encabezado_sign-in">
-                        Sign in
-                    </a>
-                    <a href="#" class="encabezado__link active">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/header-empty-card.svg" alt="cart">
-                    </a>
-                </div>
-            </div>
-            <div class="encabezado__menu-responsive">
-                <?php wp_nav_menu(
-                        array(
-                            "menu" => 'menu-responsive'
-                        )
-                    ); ?>
-            </div>
-        </div>
-    </header>
+    <?php get_template_part('template-parts/content','header'); ?>
 
     <main class="productos">
         <div class="container-fluid gx-5">
